958e5ff214261d5d004d0663ace4a9c6e3e7e870
Esteban-Piña <esteban0400@gmail.com>
2022-10-11 17:17:48 -0500

    Retiro /api

--- index.js
+++ index.js
@@ -37,12 +37,12 @@ app.use(function(req,res,next){
     next()
 })*/
 
-app.get('/api', (req, res) => {
+/*app.get('/api', (req, res) => {
     const path = `/api/item/${v4()}`;
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-  });
+  });*/
 
 //Middleware de tercero
 /*if(app.get('env')==='development'){
